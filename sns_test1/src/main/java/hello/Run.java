c5e147fcdf5215a33dda236625b7cb8787f2cac0
654193549@qq.com <654193549@qq.com>
2018-05-25 16:29:07 +0800

    试试

--- sns_test1/src/main/java/hello/Run.java
+++ sns_test1/src/main/java/hello/Run.java
@@ -7,8 +7,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //mybatis reload xml
 @MapperScan("hello.test.dao")
-public class Run {
+public class Run{
+	
+	
+//	/**
+//	 * 可以将其项目达成war包只需:
+//	 * 1.extends SpringBootServletInitializer
+//	 * 2.重写configure
+//	 * 3.修改pom.xml中的jar为war
+//	 * @param application
+//	 * @return
+//	 */
+//	//extends SpringBootServletInitializer
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//	return application.sources(Run.class);
+//	}
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Run.class, args);
 	}
